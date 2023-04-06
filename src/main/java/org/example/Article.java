b9736a385a3d07b7a508009e055d79687ec652d1
ahmedtechm <ahmedtechm2050@gmail.com>
2023-04-06 11:58:32 +0400

    Created Article Class with Basic Properties

---
+++ src/main/java/org/example/Article.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Article {
+
+    private int ID;
+    private String title;
+    private String author;
+    private String date;
+    private String category;
+    private String content;
+}
